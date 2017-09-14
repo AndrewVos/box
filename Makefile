81edad02a404fda10832a1ade89e962e5b9ab09d
Andrew Vos <andrew.vos@gmail.com>
2017-09-14 14:10:44 +0100

    Add missing assertion

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ test: $(TESTS)
 
 $(TESTS): image
 	@sudo docker run -t box-test /bin/bash "-c" "./$@"
+	@echo
 
 image:
 	@sudo docker build -q -t box-test .
--- tests/apt-test.sh
+++ tests/apt-test.sh
@@ -6,6 +6,9 @@ source tests/helpers.sh
 result=$(satisfy apt "git" 2> /dev/null)
 expect-result-to-include "apt git -> missing"
 
+result=$(dpkg --get-selections)
+expect-result-to-include "git						install"
+
 result=$(satisfy apt "git")
 expect-result-to-include "apt git -> latest"
 
